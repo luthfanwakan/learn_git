085baeea53321e153c39e135ac8314857bcae37d
slsrzka <rizkasalsa1801@gmail.com>
2025-01-14 14:02:14 +0700

    Deret Fibonacci Salsa

---
+++ salsabillarzk_.py
@@ -0,0 +1,8 @@
+n=8
+deret_fibonacci=[]
+for i in range(n):
+    if i<2 :
+        deret_fibonacci.append(1)
+    else :
+        deret_fibonacci.append(deret_fibonacci[i-1]+deret_fibonacci[i-2])
+print(deret_fibonacci)
